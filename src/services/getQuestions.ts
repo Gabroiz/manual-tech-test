d2ddd52c30cc690c3c9b6f24e112084f82210d93
Gabriel Queiroz <gabroiz@MacBook-Pro-de-Gabriel.local>
2024-02-18 14:37:08 -0300

    fix: add URL resolver

--- src/services/getQuestions.ts
+++ src/services/getQuestions.ts
@@ -1,6 +1,9 @@
 import { QuestionType } from "@/@types/question";
+import { api } from "./api";
 
 export async function getQuestions(): Promise<QuestionType[]> {
-  const response = await fetch("http://localhost:3000/api/v1/questions");
-  return response.json();
+  const response = api("/questions")
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+  return response;
 }
